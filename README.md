0c16f0db85c0abae2ea908f1194a739eee8457b6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-20 09:58:30 +0100

    Correct setup, filenames in MCMC, go AppVeyor

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # pirouette_example_16
 
-Branch   |[![Travis CI logo](pics/TravisCI.png)](https://travis-ci.org)
----------|---------------------------------------
-`master` |[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_16.svg?branch=master)](https://travis-ci.org/richelbilderbeek/pirouette_example_16)
-`develop`|[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_16.svg?branch=develop)](https://travis-ci.org/richelbilderbeek/pirouette_example_16)
+Branch   |[![Travis CI logo](pics/TravisCI.png)](https://travis-ci.org)                                                                                               |[![AppVeyor logo](pics/AppVeyor.png)](https://appveyor.com)                                                                                               
+---------|------------------------------------------------------------------------------------------------------------------------------------------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+`master` |[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_16.svg?branch=master)](https://travis-ci.org/richelbilderbeek/pirouette_example_16) |[![Build status](https://ci.appveyor.com/api/projects/status/3nvk5jt46lakbax5/branch/master?svg=true)](https://ci.appveyor.com/project/richelbilderbeek/pirouette-example-16/branch/master)
+`develop`|[![Build Status](https://travis-ci.org/richelbilderbeek/pirouette_example_16.svg?branch=develop)](https://travis-ci.org/richelbilderbeek/pirouette_example_16)|[![Build status](https://ci.appveyor.com/api/projects/status/3nvk5jt46lakbax5/branch/develop?svg=true)](https://ci.appveyor.com/project/richelbilderbeek/pirouette-example-16/branch/develop)
 
 A [pirouette example](https://github.com/richelbilderbeek/pirouette_examples).
 
--- example_16.R
+++ example_16.R
@@ -5,8 +5,6 @@
 #
 #
 
-# Set the RNG seed
-rng_seed <- 314
 
 library(pirouette)
 suppressMessages(library(ggplot2))
@@ -14,6 +12,7 @@ library(beautier)
 
 root_folder <- getwd()
 example_no <- 16
+rng_seed <- 314
 example_folder <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
 dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
 setwd(example_folder)
@@ -32,11 +31,21 @@ alignment_params <- create_alignment_params(
   ),
   fasta_filename = "alignment_gen.fasta",
   root_sequence = create_blocked_dna(length = 1000),
-  rng_seed = rng_seed
+  rng_seed = rng_seed,
+  fasta_filename = "true_alignment.fas"
 )
 
-experiment <- create_gen_experiment()
-experiments <- list(experiment)
+# JC69, strict, Yule
+generative_experiment <- create_gen_experiment()
+generative_experiment$beast2_options$input_filename <- "true_alignment_gen.xml"
+generative_experiment$beast2_options$output_state_filename <- "true_alignment_gen.xml.state"
+generative_experiment$inference_model$mcmc$tracelog$filename <- "true_alignment_gen.log"
+generative_experiment$inference_model$mcmc$treelog$filename <- "true_alignment_gen.trees"
+generative_experiment$inference_model$mcmc$screenlog$filename <- "true_alignment_gen.csv"
+generative_experiment$errors_filename <- "true_errors_gen.csv"
+check_experiment(generative_experiment)
+
+experiments <- list(generative_experiment)
 
 # Set the RNG seed
 for (i in seq_along(experiments)) {
@@ -44,23 +53,31 @@ for (i in seq_along(experiments)) {
 }
 
 # Testing
-if (1 == 1) {
+if (1 == 2) {
   for (i in seq_along(experiments)) {
     experiments[[i]]$inference_model$mcmc <- create_mcmc(chain_length = 20000, store_every = 1000)
   }
 }
 
+
+
+twinning_params <- create_twinning_params(
+  rng_seed_twin_tree = rng_seed,
+  sim_twin_tree_fun = create_copy_twin_tree_from_true_fun(),
+  rng_seed_twin_alignment = rng_seed,
+  sim_twin_alignment_fun = get_sim_twin_alignment_with_same_n_mutation_fun(
+    mutation_rate = 0.1,
+    max_n_tries = 1000
+  ),
+  twin_tree_filename = "twin_tree.newick",
+  twin_alignment_filename = "twin_alignment.fasta",
+  twin_evidence_filename = "twin_evidence.csv"
+)
+
 pir_params <- create_pir_params(
   alignment_params = alignment_params,
   experiments = experiments,
-  twinning_params = create_twinning_params(
-    rng_seed_twin_tree = rng_seed,
-    sim_twin_tree_fun = create_copy_twin_tree_from_true_fun(),
-    rng_seed_twin_alignment = rng_seed,
-    sim_twin_alignment_fun = get_sim_twin_alignment_with_std_site_model_fun(),
-    twin_tree_filename = "tree_twin.newick",
-    twin_alignment_filename = "alignment_twin.fasta"
-  )
+  twinning_params = twinning_params
 )
 
 rm_pir_param_files(pir_params)
